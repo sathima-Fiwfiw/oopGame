0a53b313b571cd3712b213d18ce140d060d29494
Chonratti <gotilove409@gmail.com>
2024-10-13 18:36:55 +0700

    Update Ipgame.java

--- Ipgame/Ipgame.java
+++ Ipgame/Ipgame.java
@@ -8,17 +8,16 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import javax.swing.ImageIcon;
 
 
 
 class Ipgame extends JFrame {
-public static void main(String[] args) {
-   Ipgame gg=new Ipgame();
-   gg.setVisible(true);
-}
-    CustomPanel jPanel = new CustomPanel();
+
+    CustomPanel jPanel = new CustomPanel(this);
 
     Ipgame() {
         setSize(1440, 810);
@@ -29,7 +28,9 @@ public static void main(String[] args) {
         jPanel.setLayout(null);
         add(jPanel, BorderLayout.CENTER);
     }
+
 }
+    
 
 class CustomPanel extends JPanel {
     Image bg;
@@ -38,8 +39,10 @@ class CustomPanel extends JPanel {
     JButton[] buttons = new JButton[5]; // Array to hold 5 buttons
     JButton enter;
     JButton back;
+    Ipgame ipgame;
 
-    CustomPanel() {
+    CustomPanel(Ipgame ipgame) {
+        this.ipgame = ipgame;
         setSize(1440, 810);
         bg = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir") +
         File.separator + "imagecharacter" + File.separator + "bg.png");
@@ -70,17 +73,29 @@ class CustomPanel extends JPanel {
 
             ImageIcon icon = new ImageIcon(System.getProperty("user.dir") + 
             File.separator + "imagecharacter" + File.separator + "enter.png"); 
-            Image icoImage = icon.getImage().getScaledInstance(1280, 700, Image.SCALE_SMOOTH);
+            Image icoImage = icon.getImage().getScaledInstance(1180, 700,Image.SCALE_SMOOTH);
             enter =new JButton(new ImageIcon(icoImage));
             enter.setBounds(600, 630, 150, 90);
             add(enter);
 
             ImageIcon icon2 = new ImageIcon(System.getProperty("user.dir") + 
             File.separator + "imagecharacter" + File.separator + "back.png"); 
-            Image icoImage2 = icon2.getImage().getScaledInstance(100, 50, Image.SCALE_SMOOTH);
+            Image icoImage2 = icon2.getImage().getScaledInstance(80, 60, Image.SCALE_SMOOTH);
             back =new JButton(new ImageIcon(icoImage2));
             back.setBounds(0, 700, 90, 60);
             add(back);
+
+            back.addActionListener(new ActionListener() {
+
+             
+             
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    menugame main = new menugame();
+                    main.setVisible(true);
+                    ipgame.dispose();
+                
+    }});
     }
 
     @Override
