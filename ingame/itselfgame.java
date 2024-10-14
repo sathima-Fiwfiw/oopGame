993de6758524f06c6387b8f719704387a634a033
Poder2004 <66011212129@msu.ac.th>
2024-10-14 22:39:36 +0700

    Update itselfgame.java

--- ingame/itselfgame.java
+++ ingame/itselfgame.java
@@ -12,29 +12,38 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import java.awt.Font;
 
 public class itselfgame extends JFrame {
-    String textcha;
+    String characterID;
+    String nameUser;
+    String IP;
     PanelGame pInGame;
+    tradetime timecount;
 
-    itselfgame(String textcha) {
-        this.textcha = textcha;
+    itselfgame(String characterID,String nameUser,String IP) {
+        this.characterID = characterID;
+        this.nameUser = nameUser;
+        this.IP = IP;
+
         setSize(1450, 840);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pInGame = new PanelGame(this);
+        timecount = new tradetime(2,0);
+        pInGame = new PanelGame(this,timecount);
         pInGame.addMouseMotionListener(pInGame);
         pInGame.addMouseListener(pInGame);
         add(pInGame);
     }
 
     public static void main(String[] args) {
-        new itselfgame("c02").setVisible(true);
+        new itselfgame("c01","PODER_555","192.0011255").setVisible(true);
     }
 }
 
 class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
     itselfgame ingame;
+    tradetime timecount;
     Image[] character = new Image[5];
     Image bg, hand;
     int movecharacter = 0, characterY = 540;
@@ -42,19 +51,24 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
     int c01Width = 200, c02Width = 150, c03Width = 150, c04Width = 120, c05Width = 160;
     boolean isJumping = false, ishand = true;
     Timer actionTimer;
+    Timer handTimer;
     int handX = 0, handY = 710;
     int handWidth = 80, handHeight = 100;
     Random random = new Random();
     private Robot robot;
+    
 
-    PanelGame(itselfgame ingame) {
+    PanelGame(itselfgame ingame,tradetime timecount) {
         this.ingame = ingame;
+        this.timecount = timecount;
+
         bg = Toolkit.getDefaultToolkit().getImage("C:/oopGame/ingame/bgingame.png");
         hand = Toolkit.getDefaultToolkit().getImage("C:/oopGame/ingame/handgrost.png");
 
         for (int i = 0; i < character.length; i++) {
             character[i] = new ImageIcon("C:/oopGame/imageip/" + (i + 1) + ".png").getImage();
         }
+        timecount.startdown();
 
         handX = random.nextInt(1100) + 10;
 
@@ -86,6 +100,17 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
 
         actionTimer.start(); // เริ่ม Timer
 
+        handTimer = new Timer(3000, new ActionListener() { 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ishand = !ishand; // สลับสถานะการแสดงมือ
+                if (ishand) {
+                    handX = random.nextInt(1100) + 10; // กำหนดตำแหน่งมือใหม่แบบสุ่ม
+                }
+                repaint(); // วาดใหม่ทุกครั้ง
+            }
+        });
+        handTimer.start(); // เริ่ม timer ของมือ
 
         try {
             robot = new Robot(); // สร้าง Robot หนึ่งตัวเพื่อควบคุมเมาส์
@@ -100,14 +125,27 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
         g.drawImage(bg, 0, 0, this);
         Image currentCharacter = getCurrentCharacter();
         g.drawImage(currentCharacter, movecharacter - getCharacterOffset(), characterY, getCharacterWidth(), characterHeight, this);
-
+        Font font = new Font("Berlin sans FB Demi", Font.BOLD, 20); 
+        if ("c01".equals(ingame.characterID)) {
+            g.setFont(font); // ตั้งค่าฟ้อนให้กับ Graphics
+             g.drawString(ingame.nameUser, movecharacter - getCharacterOffset() + 50, characterY - 20);
+        }
+        else{
+            g.setFont(font); // ตั้งค่าฟ้อนให้กับ Graphics
+            g.drawString(ingame.nameUser, movecharacter - getCharacterOffset() + 20, characterY - 20);
+        }
+        Font fonttime = new Font("Berlin sans FB Demi", Font.BOLD, 40); 
+        g.setFont(fonttime);
+        g.drawString(String.format("%02d:%02d", timecount.minutes, timecount.seconds), 300, 55);
+        g.drawString(" 001500", 1110, 55);
+       
         if (ishand) {
             g.drawImage(hand, handX, handY, handWidth, handHeight, this);
         }
     }
 
     private Image getCurrentCharacter() {
-        switch (ingame.textcha) {
+        switch (ingame.characterID) {
             case "c01": return character[0];
             case "c02": return character[1];
             case "c03": return character[2];
@@ -118,7 +156,7 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
     }
 
     private int getCharacterWidth() {
-        switch (ingame.textcha) {
+        switch (ingame.characterID) {
             case "c01": return c01Width;
             case "c02": return c02Width;
             case "c03": return c03Width;
@@ -129,12 +167,12 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
     }
 
     private int getCharacterOffset() {
-        switch (ingame.textcha) {
+        switch (ingame.characterID) {
             case "c01": return 100;
             case "c02": return 80;
-            case "c03": return 90;
-            case "c04": return 95;
-            case "c05": return 125;
+            case "c03": return 80;
+            case "c04": return 80;
+            case "c05": return 80;
             default: return 0;
         }
     }
@@ -144,33 +182,30 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
         int handLeft = handX;
         int handRight = handX + handWidth;
         int handTop = handY;
-        int handBottom = handY + handHeight; 
 
-       // System.out.println("handLeft " + handLeft + "--------"+"handRight "+handRight);
+        //System.out.println("handLeft " + handLeft + "--------"+"handRight "+handRight);
       //  System.out.println("handTop " + handTop );
         // ขอบของตัวละคร
         int characterLeft = movecharacter ;
         int characterRight = movecharacter + getCharacterWidth();
         int characterBottom = characterY + characterHeight ;
-     // System.out.println("characterLeft " + characterLeft + "--------"+"characterRight "+characterRight);
-     //  System.out.println("characterBottom " + characterBottom );
-      // System.out.println("characterY: " + characterY + ", handTop: " + handTop + ", handBottom: " + handBottom);
+       // System.out.println("characterLeft " + characterLeft + "--------"+"characterRight "+characterRight);
 
-        // ตรวจสอบการชนซ้ายขวา
-        if (characterRight - 115 > handLeft && characterLeft - 73 < handRight && characterBottom  > handTop) {
-            // ชนจากซ้ายไปขวา (ตัวละครเข้าไปด้านซ้ายของมือ)
-            if (characterRight - 115 > handLeft && characterLeft < handLeft) {
-                movecharacter -= 100; // เลื่อนตัวละครออกทางซ้าย
-                moveMouseWithCharacter(-100);
-            } 
-            // ชนจากขวาไปซ้าย (ตัวละครเข้าไปด้านขวาของมือ)
-            else if (characterLeft - 73 < handRight && characterRight > handRight) {
-               movecharacter += 100 ; // เลื่อนตัวละครออกทางขวา
-              moveMouseWithCharacter(100);
-            }
+                // ตรวจสอบการชนซ้ายขวา
+            if (characterRight - 115 > handLeft && characterLeft - 65 < handRight && characterBottom  > handTop) {
+                // ชนจากซ้ายไปขวา (ตัวละครเข้าไปด้านซ้ายของมือ)
+                if (characterRight - 115 > handLeft && characterLeft < handLeft) {
+                    movecharacter -= 100; // เลื่อนตัวละครออกทางซ้าย
+                    moveMouseWithCharacter(-100);
+                } 
+                // ชนจากขวาไปซ้าย (ตัวละครเข้าไปด้านขวาของมือ)
+                else if (characterLeft - 65 < handRight && characterRight > handRight) {
+                movecharacter += 100 ; // เลื่อนตัวละครออกทางขวา
+                moveMouseWithCharacter(100);
+                }
                 return true; // มีการชน
             }
-            
+        
         return false; // ไม่มีการชน
     }
 
@@ -192,7 +227,7 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
     public void mouseMoved(MouseEvent e) {
         movecharacter = e.getX();
          // ตัวละคร c01 - เช็คขอบ
-         if ("c01".equals(ingame.textcha)) {
+         if ("c01".equals(ingame.characterID)) {
             if (movecharacter - 100 < 0) { // ขอบซ้าย
                 movecharacter = 100;
             }
@@ -201,7 +236,7 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
             }
         }
         // ตัวละคร c02 - เช็คขอบ
-        else if ("c02".equals(ingame.textcha)) {
+        else if ("c02".equals(ingame.characterID)) {
             if (movecharacter - 80 < 0) { // ขอบซ้าย
                 movecharacter = 80;
             }
@@ -210,7 +245,7 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
             }
         }
         // ตัวละคร c03 - เช็คขอบ
-        else if ("c03".equals(ingame.textcha)) {
+        else if ("c03".equals(ingame.characterID)) {
             if (movecharacter - 90 < 0) { // ขอบซ้าย
                 movecharacter = 90;
             }
@@ -219,7 +254,7 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
             }
         }
         // ตัวละคร c04 - เช็คขอบ
-        else if ("c04".equals(ingame.textcha)) {
+        else if ("c04".equals(ingame.characterID)) {
             if (movecharacter - 95 < 0) { // ขอบซ้าย
                 movecharacter = 95;
             }
@@ -228,7 +263,7 @@ class PanelGame extends JPanel implements MouseMotionListener, MouseListener {
             }
         }
         // ตัวละคร c05 - เช็คขอบ
-        else if ("c05".equals(ingame.textcha)) {
+        else if ("c05".equals(ingame.characterID)) {
             if (movecharacter - 125 < 0) { // ขอบซ้าย
                 movecharacter = 125;
             }
