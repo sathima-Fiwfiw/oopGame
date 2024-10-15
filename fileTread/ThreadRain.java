0d18d03ec4bd0eca1e8d155532c0040e19076f81
Poder2004 <66011212129@msu.ac.th>
2024-10-16 01:06:30 +0700

    Update ThreadRain.java

--- fileTread/ThreadRain.java
+++ fileTread/ThreadRain.java
@@ -1,7 +1,7 @@
 import javax.swing.JPanel;
 
 class ThreadRain extends Thread {
-    private final PanelGame panelGame; // อ้างอิงถึง PanelGame เพื่อเข้าถึงข้อมูลของลูกอม
+    PanelGame panelGame; // อ้างอิงถึง PanelGame เพื่อเข้าถึงข้อมูลของลูกอม
     tradetime tt ;
     ThreadRain(PanelGame panelGame ,   tradetime tt) {
         this.panelGame = panelGame;
@@ -12,15 +12,32 @@ class ThreadRain extends Thread {
     public void run() {
         while (tt.isend) {
             // อัปเดตตำแหน่ง Y ของลูกอม
+                panelGame.donutY  += panelGame.donutSpeed;
+                panelGame.pumpkinY += panelGame.pumpkinSpeed;
+
+                if (panelGame.donutY > panelGame.getHeight() || !panelGame.isdonut) {
+                    panelGame.isdonut = true;
+                    panelGame.donutX =  panelGame.random.nextInt(1350)+5;
+                    panelGame.donutY = 70;
+                    panelGame.donutSpeed =  panelGame.random.nextDouble(6.0)+2.5;
+                }
+                if (panelGame.pumpkinY  > panelGame.getHeight() || !panelGame.ispumpkin) {
+                    panelGame.ispumpkin = true;
+                    panelGame.pumpkinX =  panelGame.random.nextInt(1350)+5;
+                    panelGame.pumpkinY = 70;
+                    panelGame.pumpkinSpeed =  panelGame.random.nextDouble(6.0)+2.5;
+                }
+
             for (int i = 0; i < panelGame.Candy; i++) {
                 panelGame.Rany[i] += panelGame.ranspeed[i]; // ลดตำแหน่ง Y ของลูกอม
 
                 // ตรวจสอบว่าลูกอมตกถึงด้านล่างของหน้าจอ
                 if (panelGame.Rany[i] > panelGame.getHeight() || !panelGame.iscandy[i]) {
                     panelGame.iscandy[i] = true; 
-                    panelGame.Rany[i] = 60; // รีเซ็ตตำแหน่ง Y ของลูกอม
+                    panelGame.Rany[i] = 70; // รีเซ็ตตำแหน่ง Y ของลูกอม
                     panelGame.Ranx[i] = panelGame.random.nextInt(1350) + 5; // รีเซ็ตตำแหน่ง X ของลูกอม
                     panelGame.ranspeed[i] = panelGame.random.nextDouble(10.0)+4.0; //สุ่มความเร็วใหม่ด้วย
+
                 }
             }
 
