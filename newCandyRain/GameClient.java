3db328bc8d703dc8aabb0c490acde7a15a7005ca
Chonratti <gotilove409@gmail.com>
2024-10-20 02:04:37 +0700

    Add files via upload

---
+++ newCandyRain/GameClient.java
@@ -0,0 +1,332 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.File;
+import java.net.Socket;
+import java.util.HashMap;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.util.Map; 
+import java.awt.Font;
+
+public class GameClient extends JFrame {
+    private static final int SERVER_PORT = 12345; // พอร์ตที่ใช้ในการเชื่อมต่อกับเซิร์ฟเวอร์
+
+    private JPanel panel;
+    int movecharacter = 0, characterY = 540;
+    private Socket socket; // ใช้เชื่อมต่อกับเซิร์ฟเวอร์
+    private PrintWriter out; // ใช้ส่งข้อมูลออกไปยังเซิร์ฟเวอร์
+    private Map<String, PlayerPoint> otherPlayers = new HashMap<>(); // แผนที่สำหรับเก็บตำแหน่งของผู้เล่นคนอื่น
+    private String playerName; // Name of this player
+    private String characterCode; // Character code
+    private String serverIP; // Server IP address
+    Image bg;
+    Image[] character = new Image[5];
+    boolean isJumping = false;
+    Timer actionTimer;
+    private int remainingMinutes;
+    private int remainingSeconds;
+    int Candys=15;
+    Image[] Candy =new Image[Candys];
+    int posicandyX[] =new int[Candys];
+    int posicandyY[] =new int[Candys];
+    double speedcandy[] =new double[Candys];
+    int indexcandy;
+    
+
+    // Adjusted constructor to accept name, character code, and IP from Ipgame
+    public GameClient(String characterCode, String playerName, String serverIP) {
+        this.playerName = playerName;
+        this.characterCode = characterCode;
+        this.serverIP = serverIP;
+
+        // โหลดภาพพื้นหลังและตัวละคร
+        bg = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir") +
+                File.separator + "imageingame" + File.separator + "bgingame.png");
+
+        for (int i = 0; i < character.length; i++) {
+            character[i] = new ImageIcon("D:\\week_server\\imagecharacter/" + (i + 1) + ".png").getImage();
+        }
+        for (int i = 0; i < Candys; i++) {
+            Candy[i] = new ImageIcon("D:\\week_server\\imageRain/" + (i + 1) + ".png").getImage();
+        }
+
+        createAndShowGUI(); // สร้าง GUI และแสดง
+        connectToServer(); // เชื่อมต่อกับเซิร์ฟเวอร์
+
+        actionTimer = new Timer(15, new ActionListener() {
+               boolean jumpingUp = true; // ทิศทางการกระโดด
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (isJumping) {
+                    if (jumpingUp) {
+                        characterY -= 10; // Move up
+                        if (characterY <= 385) {
+                            jumpingUp = false; // Change direction when reaching peak
+                        }
+                    } else {
+                        characterY += 10; // Move down
+
+                        if (characterY >= 540) { // Landing on the ground
+                            characterY = 540;
+                            isJumping = false;
+                            jumpingUp = true; // Reset jump direction
+                        }
+                    }
+
+                }
+                sendPosition(); 
+                panel.repaint(); // Repaint the panel every tick
+            }
+        });
+        actionTimer.start(); // Start the timer
+
+    }
+
+    private void createAndShowGUI() {
+        setTitle("Game Client");
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                // Draw the current player
+                g.drawImage(bg, 0, 0, this);
+                g.drawImage(character[getCharacterIndex(characterCode)], movecharacter - getCharacterOffset(characterCode), characterY, getCharacterWidth(characterCode), 250, this); // วาดตัวผู้เล่น
+                g.setColor(Color.BLACK);
+                g.drawString(playerName, movecharacter, characterY - 5); // วาดชื่อผู้เล่นเหนือรูปตัวละคร
+            
+                  // วาดลูกอมที่ตำแหน่งที่ได้รับจากเซิร์ฟเวอร์
+                    for (int i = 0; i < Candys; i++) {
+                        g.drawImage(Candy[i], posicandyX[i], posicandyY[i], 100, 100, this);
+                    }
+
+
+                // วาดผู้เล่นคนอื่น
+                for (Map.Entry<String, PlayerPoint> entry : otherPlayers.entrySet()) {
+                    PlayerPoint p = entry.getValue();
+                    String otherName = entry.getKey();
+
+                    Image otherCharacterImage = getCharacterImageBasedOnCode(p.characterCode); // ใช้ characterCode ของผู้เล่นอื่น
+                    g.drawImage(otherCharacterImage, p.x - getCharacterOffset(p.characterCode), p.y, getCharacterWidth(p.characterCode), 250, this); // วาดตัวผู้เล่น
+                    g.drawString(otherName, p.x, p.y - 5); // วาดชื่อผู้เล่นเหนือรูปตัวละคร
+                }
+
+                // Draw the current time
+                Font fonttime = new Font("Berlin sans FB Demi", Font.BOLD, 40); 
+                g.setFont(fonttime);
+                g.drawString(String.format("%02d:%02d", remainingMinutes, remainingSeconds), 300, 55);
+
+                repaint();
+            }
+        };
+
+        panel.setPreferredSize(new Dimension(1440, 810));
+        add(panel);
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Mouse motion listener to track mouse movement
+        panel.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                movecharacter = e.getX(); // Center the character on the mouse, using offset
+
+                // ตัวละคร c01 - เช็คขอบ
+            if ("c01".equals(characterCode)) {
+                if (movecharacter - 100 < 0) { // ขอบซ้าย
+                    movecharacter = 100;
+                }
+                if (movecharacter + 90 > panel.getWidth()) { // ขอบขวา
+                    movecharacter = panel.getWidth() - 90;
+                }
+            }
+            // ตัวละคร c02 - เช็คขอบ
+            else if ("c02".equals(characterCode)) {
+                if (movecharacter - 80 < 0) { // ขอบซ้าย
+                    movecharacter = 80;
+                }
+                if (movecharacter + 65 > panel.getWidth()) { // ขอบขวา
+                    movecharacter = panel.getWidth() - 65;
+                }
+            }
+            // ตัวละคร c03 - เช็คขอบ
+            else if ("c03".equals(characterCode)) {
+                if (movecharacter - 90 < 0) { // ขอบซ้าย
+                    movecharacter = 90;
+                }
+                if (movecharacter + 65 > panel.getWidth()) { // ขอบขวา
+                    movecharacter = panel.getWidth() - 65;
+                }
+            }
+            // ตัวละคร c04 - เช็คขอบ
+            else if ("c04".equals(characterCode)) {
+                if (movecharacter - 95 < 0) { // ขอบซ้าย
+                    movecharacter = 95;
+                }
+                if (movecharacter + 30 > panel.getWidth()) { // ขอบขวา
+                    movecharacter = panel.getWidth() - 30;
+                }
+            }
+            // ตัวละคร c05 - เช็คขอบ
+            else if ("c05".equals(characterCode)) {
+                if (movecharacter - 80 < 0) { // ขอบซ้าย
+                    movecharacter = 80;
+                }
+                if (movecharacter + 70 > panel.getWidth()) { // ขอบขวา
+                    movecharacter = panel.getWidth() - 70;
+                }
+            }
+              
+                sendPosition(); // ส่งตำแหน่งผู้เล่นไปยังเซิร์ฟเวอร์
+                panel.repaint();
+            }
+        });
+
+                // เพิ่ม mouseClicked() สำหรับการตรวจจับคลิกเมาส์
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                    if (!isJumping) {
+                        isJumping = true; // กระโดดเมื่อไม่ได้อยู่ในสถานะการกระโดด
+                    }
+            }
+        });
+    }
+
+        // เพิ่มเมธอด getCharacterWidth() และ getCharacterOffset()
+        private int getCharacterWidth(String characterCodeID) {
+            switch (characterCodeID) { 
+                case "c01": return 200;
+                case "c02": return 150;
+                case "c03": return 150;
+                case "c04": return 120;
+                case "c05": return 160;
+                default: return 0;
+            }
+        }
+
+        private int getCharacterOffset(String characterCodeID) {
+            switch (characterCodeID) {
+                case "c01": return 100;
+                case "c02": return 80;
+                case "c03": return 80;
+                case "c04": return 80;
+                case "c05": return 80;
+                default: return 0;
+            }
+        }
+
+
+    private void connectToServer() {
+        try {
+            socket = new Socket(serverIP, SERVER_PORT); // เชื่อมต่อไปยังเซิร์ฟเวอร์ที่กำหนด
+            out = new PrintWriter(socket.getOutputStream(), true); // สร้าง PrintWriter เพื่อส่งข้อมูลออกไปยังเซิร์ฟเวอร์
+            out.println(playerName); // ส่งชื่อผู้เล่นไปยังเซิร์ฟเวอร์
+            out.println(characterCode); // ส่งรหัสตัวละครไปยังเซิร์ฟเวอร์
+            new Thread(this::receivePositionUpdates).start(); // สร้าง thread ใหม่เพื่อรับข้อมูลจากเซิร์ฟเวอร์
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendPosition() {
+        out.println(playerName + "," + movecharacter + "," + characterY + "," + characterCode); // ส่งชื่อผู้เล่น ตำแหน่ง x, y และโค้ดตัวละคร
+    }
+
+    private void updateTimerDisplay(int minutes, int seconds) {
+        this.remainingMinutes = minutes;
+        this.remainingSeconds = seconds;
+    }
+
+    private void updateCandyPosition(int x, int y,int index,double speed) {
+        this.posicandyX[index] = x;
+        this.speedcandy[index] = speed;
+        this.posicandyY[index] = (int) (y + speedcandy[index]); 
+        panel.repaint(); // Repaint the panel to update candy position
+    }
+
+    private void receivePositionUpdates() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            String message;
+            while ((message = in.readLine()) != null) {
+                String[] parts = message.split(","); // แยกข้อมูลที่ได้รับเป็นส่วนๆ
+                if (parts.length == 4) {
+                    String otherPlayerName = parts[0];
+                    int otherX = Integer.parseInt(parts[1]);
+                    int otherY = Integer.parseInt(parts[2]);
+                    String otherCharacterCode = parts[3];
+
+                    // อัปเดตตำแหน่งของผู้เล่นคนอื่น
+                    PlayerPoint playerPoint = new PlayerPoint(otherX, otherY, otherCharacterCode);
+                    otherPlayers.put(otherPlayerName, playerPoint); // เก็บข้อมูลผู้เล่นอื่นลงในแผนที่
+                    panel.repaint(); // วาดใหม่เพื่ออัปเดตการแสดงผล
+                }
+                else if (parts[0].equals("time")) {
+                    // Update time
+                    int minutes = Integer.parseInt(parts[1]);
+                    int seconds = Integer.parseInt(parts[2]);
+                    updateTimerDisplay(minutes, seconds); // Update the timer display
+                }  else if (parts[0].equals("candy")) {
+                    // อัปเดตตำแหน่งและความเร็วลูกอมที่ได้รับจากเซิร์ฟเวอร์
+                    int candyIndex = Integer.parseInt(parts[1]); // รับตำแหน่ง index ของลูกอม
+                    int candyX = Integer.parseInt(parts[2]); // รับตำแหน่ง x
+                    int candyY = Integer.parseInt(parts[3]); // รับตำแหน่ง y
+                    double candySpeed = Double.parseDouble(parts[4]); // รับความเร็วของลูกอม
+                
+                    // อัปเดตตำแหน่งลูกอมในแผงหรือพื้นที่แสดงผล
+                    updateCandyPosition(candyX, candyY, candyIndex,candySpeed);
+                    panel.repaint();
+                }
+                panel.repaint(); 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+     // เมธอดช่วยในการเลือกรูปตัวละครตามรหัส
+     private Image getCharacterImageBasedOnCode(String code) {
+        int index = getCharacterIndex(code);
+        return character[index];
+    }
+
+    private int getCharacterIndex(String code) {
+        switch (code) {
+            case "c02":
+                return 1;
+            case "c03":
+                return 2;
+            case "c04":
+                return 3;
+            case "c05":
+                return 4;
+            default:
+                return 0;
+        }
+    }
+
+     // คลาสย่อยสำหรับเก็บตำแหน่งและโค้ดตัวละครของผู้เล่น
+     private static class PlayerPoint {
+        int x, y;
+        String characterCode;
+        PlayerPoint(int movecharacter, int characterY, String characterCode) {
+            this.x = movecharacter;
+            this.y = characterY;
+            this.characterCode = characterCode;
+        }
+    }
+}
---
+++ newCandyRain/GameServer.java
@@ -0,0 +1,162 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class GameServer {
+    private static final int SERVER_PORT = 12345;
+    private static final Set<PrintWriter> clientWriters = new HashSet<>();
+    tradetime timecount;
+    ThreadRain threadRain; // เพิ่มการใช้งาน ThreadRain
+
+    GameServer(){
+        timecount = new tradetime(0,50); 
+        threadRain = new ThreadRain(timecount); // เรียกใช้งาน ThreadRain
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Game Server is running...");
+        new GameServer().startServer();
+    }
+
+    public void startServer() {
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            boolean firstClientConnected = false;
+             // เริ่มการทำงานของลูกอม
+            while (true) {
+                new ClientHandler(serverSocket.accept()).start();
+
+                // ตรวจสอบว่าถ้ามี client เข้ามาเชื่อมต่อแล้วค่อยเริ่มจับเวลา
+                if (!firstClientConnected) {
+                    timecount.startdown(); // เริ่มนับถอยหลัง
+                    startBroadcastingTime();
+                    startBroadcastingcandy();
+                    threadRain.start();
+                    firstClientConnected = true; // ตั้งค่าเป็น true หลังจาก client แรกเชื่อมต่อ
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startBroadcastingTime() {
+        new Thread(() -> {
+            while (timecount.isend) {
+                try {
+                    Thread.sleep(1000); // ส่งข้อมูลทุกวินาที
+                    broadcastTime(timecount.minutes, timecount.seconds);
+               
+                    System.out.println("Broadcasting time: " + timecount.minutes + "m " + timecount.seconds + "s");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    
+    private void broadcastTime(int minutes, int seconds) {
+        String timeMessage = "time," + minutes + "," + seconds;
+        synchronized (clientWriters) {
+            for (PrintWriter clientOut : clientWriters) {
+                clientOut.println(timeMessage);
+            }
+        }
+    }
+
+
+    private void startBroadcastingcandy() {
+        new Thread(() -> {
+            while (timecount.isend) {
+                try {
+                    Thread.sleep(20); // ส่งข้อมูลทุกวินาที
+                   for(int i=0;i<threadRain.Candy;i++){
+                    broadcastCandyPosition(i,threadRain.Ranx[i],threadRain.Rany[i],threadRain.ranspeed[i]); // ส่งตำแหน่งลูกอมทุกวินาทีด้วย
+                   }
+                   
+                  
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    // ส่งตำแหน่งลูกอมไปยัง Client ทั้งหมด
+    private void broadcastCandyPosition(int index,int x,int y,double speed) {
+        synchronized (clientWriters) {
+            for (PrintWriter clientOut : clientWriters) {
+                for ( int i = 0; i < threadRain.Candy; i++) {
+                    String candyMessage = "candy," + i + "," + threadRain.Ranx[i] + "," + threadRain.Rany[i] + "," + threadRain.ranspeed[i];
+                    clientOut.println(candyMessage);
+                }
+                // ส่งตำแหน่งและความเร็วของโดนัทและฟักทอง
+                String donutMessage = "donut," + threadRain.donutX + "," + threadRain.donutY + "," + threadRain.donutSpeed;
+                String pumpkinMessage = "pumpkin," + threadRain.pumpkinX + "," + threadRain.pumpkinY + "," + threadRain.pumpkinSpeed;
+                clientOut.println(donutMessage);
+                clientOut.println(pumpkinMessage);
+            }
+        }
+    }
+    
+
+    private class ClientHandler extends Thread {
+        private Socket socket;
+        private PrintWriter out;
+        private String playerName;
+        private String characterCode;
+        private int x, y;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                
+                synchronized (clientWriters) {
+                    clientWriters.add(out);
+                }
+
+                // Receive player name and character code
+                playerName = in.readLine();
+                characterCode = in.readLine();
+
+                String message;
+                while ((message = in.readLine()) != null) {
+                    String[] parts = message.split(",");
+                    if (parts.length == 4) {
+                        playerName = parts[0];
+                        x = Integer.parseInt(parts[1]);
+                        y = Integer.parseInt(parts[2]);
+                        characterCode = parts[3];
+                        broadcastPosition();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    // Remove the PrintWriter from the set when the player disconnects
+                    synchronized (clientWriters) {
+                        if (out != null) {
+                            clientWriters.remove(out);
+                        }
+                    }
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void broadcastPosition() {
+            synchronized (clientWriters) {
+                for (PrintWriter writer : clientWriters) {
+                    writer.println(playerName + "," + x + "," + y + "," + characterCode);
+                }
+            }
+        }
+    }
+}
